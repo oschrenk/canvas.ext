80417838dc9f4bb6befbc413004c7c282a1d4e52
Oliver Schrenk <oliver.schrenk@gmail.com>
2010-12-14 19:06:43 +0100

    Drawing rounded rectangles where each corner cas has its own radius

---
+++ canvas.js
@@ -0,0 +1,114 @@
+CanvasRenderingContext2D.prototype.roundedRectShadow = 
+ 
+/**
+ * Draws the shadow of a rounded rectangle using the current state 
+ * of the canvas.
+ * @param {Number} x
+ * 		The top left x coordinate
+ * @param {Number} y
+ * 	 	The top left y coordinate 
+ * @param {Number} width
+ * 		The width of the rectangle 
+ * @param {Number} height 
+ * 		The height of the rectangle
+ * @param {Number} radiusBottomLeft
+ * 		The corner radius of the bottom left corner. Defaults to 5;
+ * @param {Number} radiusBottomRight
+ * 		The corner radius of the bottom right corner. Defaults to 5;
+ */
+function(x, y, width, height, radiusBottomLeft, radiusBottomRight) {
+  	if (typeof radiusBottomLeft === "undefined") {
+	    radius = 5;
+	}
+	if (typeof radiusBottomRight === "undefined") {
+	    radius = 5;
+	}
+	
+	this.beginPath();
+	this.moveTo(x+width, y + height - radiusBottomRight);
+
+	// bottom right corner
+	this.quadraticCurveTo(x + width, y + height, x + width - radiusBottomRight, y + height);
+	this.lineTo(x + radiusBottomLeft, y + height);
+	
+	// bottom left corner
+	this.quadraticCurveTo(x, y + height, x, y + height - radiusBottomLeft);
+	
+	this.stroke();
+}
+
+CanvasRenderingContext2D.prototype.roundedRect = 
+ 
+/**
+ * Draws a rounded rectangle using the current state of the canvas. 
+ * @param {Number} x
+ * 		The top left x coordinate
+ * @param {Number} y
+ * 	 	The top left y coordinate 
+ * @param {Number} width
+ * 		The width of the rectangle 
+ * @param {Number} height 
+ * 		The height of the rectangle
+ * @param {Number} radiusBottomLeft
+ * 		The corner radius of the bottom left corner. Defaults to 5;
+ * @param {Number} radiusBottomRight
+ * 		The corner radius of the bottom right corner. Defaults to 5;
+ * @param {Number} radiusTopRight
+ * 		The corner radius of the top right corner. Defaults to 5;
+ * @param {Number} radiusTopLeft
+ * 		The corner radius of the top left corner. Defaults to 5;
+ * @param {Boolean} fill 
+ * 		Whether to fill the rectangle. Defaults to false.
+ */
+function(x, y, width, height, radiusBottomLeft, radiusBottomRight, radiusTopRight, radiusTopLeft, fill, stroke) {
+  	if (typeof radiusBottomLeft === "undefined") {
+	    radius = 5;
+	}
+	if (typeof radiusBottomRight === "undefined") {
+	    radius = 5;
+	}
+	if (typeof radiusTopRight === "undefined") {
+	    radius = 5;
+	}
+	if (typeof radiusTopLeft === "undefined") {
+	    radius = 5;
+	}
+
+	if (typeof fill == "undefined" ) {
+	  fill = false;
+	}
+	
+	if (typeof stroke == "undefined" ) {
+	  stroke = true;
+	}
+
+	this.beginPath();
+	this.moveTo(x + radiusTopLeft, y);
+	this.lineTo(x + width - radiusTopRight, y);
+	
+	// top right corner
+	this.quadraticCurveTo(x + width, y, x + width, y + radiusTopRight);
+	this.lineTo(x + width, y + height - radiusBottomRight);
+	
+	// bottom right corner
+	this.quadraticCurveTo(x + width, y + height, x + width - radiusBottomRight, y + height);
+	this.lineTo(x + radiusBottomLeft, y + height);
+	
+	// bottom left corner
+	this.quadraticCurveTo(x, y + height, x, y + height - radiusBottomLeft);
+	this.lineTo(x, y + radiusTopLeft);
+	
+	// top left corner
+	this.quadraticCurveTo(x, y, x + radiusTopLeft, y);
+	this.closePath();
+	
+	if (stroke) {
+	  this.stroke();
+	}
+	
+	this.stroke();
+	
+	if (fill) {
+	  this.fill();
+	}        
+}
